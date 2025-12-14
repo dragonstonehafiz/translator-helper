6b54f3ff16adaeca9eff83bc13b05e0f3d521b00
dragonstonehafiz <dragonstonehafiz@gmail.com>
2025-12-14 11:49:20 +0800

    moved files around

--- test/pytorch_check.py
+++
@@ -1,7 +0,0 @@
-# sanity_check_2.py
-import torch, torchvision, platform
-print("Python :", platform.python_version())
-print("Torch   :", torch.__version__)
-print("TVision :", torchvision.__version__)
-print("CUDA OK :", torch.cuda.is_available())
-print("nms op  :", torch.ops.torchvision.nms)     # should print a C++ function handle
